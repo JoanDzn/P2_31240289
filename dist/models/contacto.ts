c7e7f3f4656fc5d6bd940e3d16a7f15165359f3e
joandzn <joan2006vg22@gmail.com>
2025-05-31 17:44:43 -0400

    task3: Third party connection

--- dist/models/contacto.ts
+++ dist/models/contacto.ts
@@ -2,11 +2,11 @@ import db from './sqlite';
 
 
 export default {
-    create1 (nombre: any, email:any, telefono:any, mensaje:any) {
+    create1 (nombre: any, email:any, telefono:any, mensaje:any, pais:any) {
         return new Promise((resolve, reject) => {
-            const sql1 = `INSERT INTO contacto (id, nombre, email, telefono, mensaje) VALUES (?, ?, ?, ?)`;
+            const sql1 = `INSERT INTO contacto (nombre, email, telefono, mensaje, pais) VALUES (?, ?, ?, ?, ?)`;
 
-            db.run(sql1, [nombre, email, telefono, mensaje], function (error) {
+            db.run(sql1, [nombre, email, telefono, mensaje, pais], function (error) {
                 if (error) reject(error);
             else resolve(this.lastID);
             });
@@ -15,7 +15,7 @@ export default {
 
     create2 (nombre: any, email:any, telefono:any, direccion:any, tarjeta:any, mes:any, ano:any, cvv:any, monto:any, moneda:any) {
         return new Promise((resolve, reject) => {
-            const sql2 = `INSERT INTO pagos (id, nombre, email, telefono, direccion, tarjeta, mes, ano, cvv, monto, moneda) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+            const sql2 = `INSERT INTO pagos (nombre, email, telefono, direccion, tarjeta, mes, ano, cvv, monto, moneda) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
 
             db.run(sql2, [nombre, email, telefono, direccion, tarjeta, mes, ano, cvv, monto, moneda], function (error) {
                 if (error) reject(error);
---
+++ dist/routes/index.ts
@@ -0,0 +1,138 @@
+import express, { Request, Response, NextFunction, Router } from 'express';
+import contacto from '../models/contacto';
+import db from '../models/sqlite';
+import axios from 'axios'
+import { v4 as uuidv4 } from 'uuid';
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+dotenv.config();
+
+let router: Router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('index', { title: 'Hola Mundo :P' });
+});
+
+
+router.get('/pagos', function(req, res) {
+    res.render('pagos', { title: 'Compra del Servicio' });
+});
+
+interface contacto {
+    id: number;
+    nombre: string;
+    email: string;
+    telefono: number;
+    mensaje: string;
+
+}
+
+router.get('/admin/contacts', function (req, res) {
+    const query = 'SELECT * FROM contacto';
+
+    db.all(query, (err: Error | null, rows: contacto[]) => {
+        if (err) {
+            console.error('Error al ejecutar la consulta:', err.message);
+            res.status(500).send('Error interno del servidor');
+            return;
+        }
+        // Renderizar la vista 'index.ejs' y pasar los resultados de la consulta
+        res.render('contacts', { title: 'Lista de contactos',contactos: rows });
+    });
+});
+
+interface RecaptchaResponse {
+    success: boolean;
+    challenge_ts?: string;
+    hostname?: string;
+    'error-codes'?: string[];
+}
+
+router.post('/contacto', async function (req, res, next) {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.userGmail,
+            pass: process.env.passwordGmail    
+        },
+        tls: {
+        rejectUnauthorized: false
+    }
+    });
+
+        const { nombre, email, telefono, mensaje } = req.body;
+        console.log(req.body);
+        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+            let pais = '';
+
+            try {
+                const ipstackKey = process.env.ipstackKey;
+                const ipstackResponse = await axios.get(`http://api.ipstack.com/${ip}?access_key=${ipstackKey}`);
+                const location = ipstackResponse.data as { country_name?: string };
+                pais = location.country_name || '';
+                console.log('Ubicación del usuario:', location);
+            } catch (err) {
+                console.error('Error consultando ipstack:', err);
+            }
+        
+    
+
+    contacto.create1(nombre, email, telefono, mensaje, pais)
+        .then(async (id) => {
+
+            const mailOptions = {
+                from: 'neverandagency@gmail.com',
+                to: 'programacion2ais@yopmail.com',
+                subject: 'Nuevo contacto desde MoviGo!',
+                text: `Nombre: ${nombre}\nEmail: ${email}\nTeléfono: ${telefono}\nMensaje: ${mensaje}\nPais: ${pais}\nIP: ${ip}`,
+            };
+
+            try {
+                await transporter.sendMail(mailOptions);
+                res.redirect('/');
+                console.log('Contacto guardado y correo enviado');
+            } catch (error) {
+                console.error('Error al guardar el contacto o enviar el correo:', error);
+                return res.status(500).send('Error al guardar el contacto o enviar el correo');
+            }
+        })
+    });
+
+
+
+
+router.post('/payments', async function (req, res, next) {
+    const {nombre, email, telefono, direccion, tarjeta, mes, ano, cvv, monto, moneda} = req.body;
+    console.log(req.body);
+        const datapayment ={
+            "amount": monto,
+            "card-number": tarjeta,
+            "cvv": cvv,
+            "expiration-month": mes,
+            "expiration-year": ano,
+            "full-name": nombre,
+            "currency": moneda,
+            "description": "Pago por servicio de MovGo!",
+            "reference": uuidv4()
+        }
+        console.log( datapayment);
+        try {
+            const paymentResponse = await axios.post('https://fakepayment.onrender.com/payments', datapayment,{ headers:{ Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiZmFrZSBwYXltZW50IiwiZGF0ZSI6IjIwMjUtMDUtMzFUMTU6MTg6NTIuODgwWiIsImlhdCI6MTc0ODcwNDczMn0.-7aFpxLfee-JVHPZXfGaXVImBklrkZeulh5MNQjDWqc' }});
+            
+            await contacto.create2(nombre, email, telefono, direccion, tarjeta, mes, ano, cvv, monto, moneda);
+
+                res.render('pagos', {
+                    title: 'Compra del Servicio',
+                    siteKey: process.env.site_key,
+                    paymentResult: paymentResponse.data
+                });
+            
+        } catch (error: any) {
+            console.error('Error en el pago:', error.response?.data || error.message);
+            res.status(500).send('Error al procesar el pago');
+        }
+});
+export default router;
+
+
