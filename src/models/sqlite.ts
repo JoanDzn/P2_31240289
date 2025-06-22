97efa9b3da0cdbf9c94996666c6e1490965b4836
joandzn <joan2006vg22@gmail.com>
2025-06-21 23:33:22 -0400

    task4

--- src/models/sqlite.ts
+++ src/models/sqlite.ts
@@ -1,18 +1,35 @@
 import path from 'path';
 import sqlite from 'sqlite3';
 
-const db = new sqlite.Database(path.resolve(__dirname, '../db', 'contacto.db'), error => {
+const db = new sqlite.Database(
+    path.resolve(__dirname, '../db', 'contacto.db'),
+     error => {
         if (error) {
             return console.log(error.message);
-        } else{
-            console.log('conexion a la Base de Datos Exitosa!!!');
+        }else {
+            console.log('Conectado a la base de datos contacto.db');
         }
 
-        const sql1 = `CREATE TABLE IF NOT EXISTS contacto (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre VARCHAR(100) NOT NULL, email TEXT, telefono INTEGER, mensaje TEXT, pais TEXT)`;
+        const contact = `CREATE TABLE IF NOT EXISTS contacto (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre VARCHAR(100) NOT NULL, email TEXT, telefono INTEGER, mensaje TEXT, ip INTEGER ,pais TEXT, fecha DATETIME DEFAULT CURRENT_TIMESTAMP)`;
 
+        const pays = `CREATE TABLE IF NOT EXISTS pagos (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre VARCHAR(100) NOT NULL, email TEXT, servicio TEXT, tarjeta INTEGER, mes INTEGER, ano INTEGER, cvv INTEGER, monto INTEGER, moneda TEXT, fecha DATETIME DEFAULT CURRENT_TIMESTAMP)`;
 
-        const sql2 = `CREATE TABLE IF NOT EXISTS pagos (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre VARCHAR(100) NOT NULL, email TEXT, telefono INTEGER, direccion TEXT, tarjeta INTEGER, mes INTEGER, ano INTEGER, cvv INTEGER, monto INTEGER, moneda TEXT)`;
-        db.run(sql1,sql2, (error) => {
+        const users = `CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR(100) NOT NULL, email TEXT, password_hash TEXT, created_at DATETIME DEFAULT CURRENT_TIMESTAMP)`;
+        db.run(contact, (error) => {
+            if (error) {
+                return console.log(error);
+            }
+            
+        });
+
+        db.run(pays, (error) => {
+            if (error) {
+                return console.log(error);
+            }
+            
+        });
+
+        db.run(users, (error) => {
             if (error) {
                 return console.log(error);
             }
